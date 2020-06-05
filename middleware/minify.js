24f4b8f4eb22143a0dd47747ca20feef754e6fae
theashishmalik <imashishmalik.45@gmail.com>
2020-06-05 18:20:22 +0530

    basic functions done

--- middleware/minify.js
+++ middleware/minify.js
@@ -1,12 +1,15 @@
 module.exports = function (req, res, next) {
     try {
-        let sourceCode=req.body.inoutText;
-
-
-    
-
-
-
+        let sourceCode=req.body.inputText;
+        // removes extra space and new lines
+        sourceCode=sourceCode.replace(/\s{2,}/g,'')
+        // handling special cases
+        sourceCode=sourceCode.replace(/ = |= | =/g,'=')
+        sourceCode=sourceCode.replace(/ , |, | ,/g,',')
+        sourceCode=sourceCode.replace(/ { |{ | {/g,'{')
+        sourceCode=sourceCode.replace(/ } |} | }/g,'}')
+        sourceCode=sourceCode.replace(/ : |: | "/g,':')
+        req.body.inputText=sourceCode
         next()
     } catch (err) {
         console.error(err)
