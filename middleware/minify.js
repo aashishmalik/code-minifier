fd77e59235061770e45fb3fad34f6007ba2120e8
theashishmalik <imashishmalik.45@gmail.com>
2020-06-12 17:12:35 +0530

    minify completed

--- middleware/minify.js
+++ middleware/minify.js
@@ -1,17 +1,115 @@
-module.exports = function (req, res, next) {
+const axios = require('axios')
+const fs = require('fs')
+const cheerio = require('cheerio')
+const Terser = require("terser")
+const CleanCSS = require('clean-css')
+const TerserHtml = require('html-minifier-terser').minify
+
+module.exports = async function (req, res, next) {
     try {
 
-        let sourceCode=req.body.inputText;
-        // removes extra space and new lines
-        sourceCode=sourceCode.replace(/\s{2,}/g,'')
-        // handling special cases
-        sourceCode=sourceCode.replace(/ = |= | =/g,'=')
-        sourceCode=sourceCode.replace(/ , |, | ,/g,',')
-        sourceCode=sourceCode.replace(/ { |{ | {/g,'{')
-        sourceCode=sourceCode.replace(/ } |} | }/g,'}')
-        sourceCode=sourceCode.replace(/ : |: | "/g,':')
-        req.body.inputText=sourceCode
-        next()
+        const html = await axios.get(`${req.body.inputText}`)
+        const $ = await cheerio.load(html.data);
+        const jsLinks = []
+        const cssLinks =[]
+        // get all scripts links
+        $('script').each((i, el) => {
+            if ($(el).attr('data-src'))
+                jsLinks.push($(el).attr('data-src'))
+            if ($(el).attr('src'))
+                jsLinks.push($(el).attr('src'))
+            $(el).remove();
+        })
+      async function performJSminification() {
+            try {
+
+                let code = {}
+                let i=1
+                const promises = jsLinks.map(url => {
+                    return axios.get(url)
+                        .then(response => {
+                            code[`file${i++}.js`] = response.data
+                        })
+                        .catch(err => {
+                            console.error(err)
+                        })
+                });
+                await Promise.all(promises);
+                let options = {
+                    toplevel: true, warnings: true, mangle: {
+                        properties: true,
+                    }
+                }
+                let result = Terser.minify(code, options);
+                fs.writeFile('./public/minihtml/js_terser.js', result.code, (err) => {
+                    if (err) {
+                        console.error(err)
+                        return res.status(500).json({ msg: "fs error" })
+                    }
+                });
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        performJSminification()
+
+
+        $('link[rel="stylesheet"]').each((i, el) => {
+             if ($(el).attr('href'))
+                cssLinks.push($(el).attr('href'))
+            if ($(el).attr('data-href'))
+                cssLinks.push($(el).attr('data-href'))
+            $(el).remove();
+        })
+        
+        async function performCSSminification() {
+            try {
+
+                let code = {}
+
+                const promisesCss = cssLinks.map(url => {
+                    return axios.get(url)
+                        .then(response => {
+                            code+=response.data
+                        })
+                        .catch(err => {
+                            console.error(err)
+                        })
+                });
+                await Promise.all(promisesCss);
+
+                let options = {level: 1};
+                let result = new CleanCSS(options).minify(code);
+
+                fs.writeFile('./public/minihtml/css_clean.css', result.styles, (err) => {
+                    if (err) {
+                        console.error(err)
+                        return res.status(500).json({ msg: "fs error" })
+                    }
+                });
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        performCSSminification()
+
+        $('<script>').attr({ src: '/js_terser.js', type: 'text/javascript' }).appendTo('body')
+        $('<link/>').attr({rel: 'stylesheet',type: 'text/css',href: 'css_clean.css'}).appendTo('head');
+        let result = TerserHtml($.html(), {
+            removeAttributeQuotes: true,
+            collapseWhitespace:true
+          });
+        fs.writeFile('./public/minihtml/test.html', result, (err) => {
+            if (err) {
+                console.error(err)
+                return res.status(500).json({ msg: "fs error" })
+            }
+        })
+        setTimeout(()=>{
+            next()
+        },5000)
     } catch (err) {
         console.error(err)
     }
--- server.js
+++ server.js
@@ -2,36 +2,34 @@ const express = require('express')
 const logger = require('morgan')
 const minify = require('./middleware/minify')
 const path = require('path')
-const fs = require('fs')
 var bodyParser = require('body-parser');
+const child_process = require('child_process');
 const app = express()
 //
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(logger('dev'))
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.post('/minified', minify, (req, res) => {
-    res.render('minified',{inputText:req.body.inputText})
+app.post('/download', minify, (req, res) => {
+    var filePath = "./public/minihtml"; 
+    child_process.execSync(`zip -r html_minified *`, {
+        cwd: filePath
+      });
+    res.download(filePath+'/archive.zip');
 })
 
 
 app.post('/render', minify, (req, res) => {
     try {
-        fs.writeFile('./public/minihtml/test.html', req.body.inputText,(err) => {
-            if (err) {
-                console.error(err)
-                return res.status(500).json({ msg: "fs error" })
-            }
-            res.sendFile(path.join(__dirname, '/public/minihtml/test.html'))
-        });
+        res.sendFile(path.join(__dirname, '/public/minihtml/test.html'))
     } catch (err) {
-        console.error(err.message)
-        return res.status(500).json({ msg: "fs error" })
+        return res.status(500).json({ msg: "file not found" })
     }
 })
 
